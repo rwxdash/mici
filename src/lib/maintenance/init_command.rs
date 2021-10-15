0db7317890dae174b8d07591ba37483a73983ef3
Oguzhan Ozdemir <aoozdemir@live.com>
2021-10-16 00:17:57 +0300

    [WIP] Include config.yml setup for init cmd

--- src/lib/maintenance/init_command.rs
+++ src/lib/maintenance/init_command.rs
@@ -1,5 +1,7 @@
+use dialoguer::{theme::ColorfulTheme, Input};
 use std::error::Error;
 use std::fs;
+use std::io::Write;
 use std::path::Path;
 use std::process;
 
@@ -10,9 +12,18 @@ use crate::PROJECT_DIR;
 extern crate colored;
 use colored::*;
 
+extern crate serde;
+use serde::{Deserialize, Serialize};
+
 #[allow(dead_code)]
 pub struct InitCommand {
     base: BaseCommand,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct InitConfiguration {
+    upstream_url: String,
+    upstream_cmd_path: String,
 }
 
 impl InitCommand {
@@ -45,7 +56,7 @@ impl InitCommand {
                 println!("  {}", "Removing ~/.minici".bright_yellow());
                 if let Err(e) = fs::remove_dir_all(&project_folder()) {
                     println!("  {}", "Error while removing ~/.minici".on_red());
-                    println!("  {}\n", e.to_string().on_red());
+                    println!("  {}", e.to_string().on_red());
                     process::exit(1)
                 } else {
                     println!("  {}", "Cleanup finished!".bright_green());
@@ -60,7 +71,7 @@ impl InitCommand {
                 println!(
                     "  {} {}",
                     "For further information, call this with".bright_black(),
-                    "--help\n".bright_yellow()
+                    "--help".bright_yellow()
                 );
 
                 return Ok(());
@@ -74,11 +85,42 @@ impl InitCommand {
                 "Error while creating".bright_red(),
                 &PROJECT_DIR.bright_red()
             );
-            println!("  {}\n", e.to_string().on_red());
+            println!("  {}", e.to_string().on_red());
 
             process::exit(1)
-        }
-        println!("  {}\n", "Successfully set up minici!".green());
+        };
+
+        let upstream_url: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!("{}", "Upstream repository URL for your commands",))
+            .interact_text()
+            .unwrap();
+        let upstream_cmd_path: String = Input::with_theme(&ColorfulTheme::default())
+            .with_prompt(format!("{}", "Path for your commands in the repository",))
+            .default("./".to_string())
+            .interact_text()
+            .unwrap();
+
+        let init_configuration = InitConfiguration {
+            upstream_url: upstream_url,
+            upstream_cmd_path: upstream_cmd_path,
+        };
+        let mut config_yaml = fs::File::create(format!("{}/config.yml", &project_folder()))?;
+        let config_yaml_as_string = serde_yaml::to_string(&init_configuration)?;
+        config_yaml.write_all(&config_yaml_as_string.as_bytes())?;
+
+        println!(
+            "> {} {}",
+            "Wrote the given configuration at",
+            &project_folder()
+        );
+        println!(
+            "  {} {}{}",
+            "You can manually update this configuration at",
+            &project_folder().blue().bold(),
+            "/config.yml".blue().bold()
+        );
+
+        println!("  {}", "Successfully set up minici!".green());
 
         return Ok(());
     }
